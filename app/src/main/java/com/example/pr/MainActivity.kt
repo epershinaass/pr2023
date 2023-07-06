aedad335c2bfa8e2872ec214c0d9522fac07d266
epershinaass <lizalizova2003@gmail.com>
2023-07-06 11:39:57 +0600

    ffirst

---
+++ app/src/main/java/com/example/pr/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.pr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+
+        }
+    }
+}
